76579ebceaed3fb9f15969ded354adefb52873a4
supunkamburugamuve <supun06@gmail.com>
2018-07-07 23:38:42 -0400

    improving the performance

--- scripts/create_small_files.py
+++ scripts/create_small_files.py
@@ -1,12 +1,22 @@
+import io
+
 filename='eRPGenerator_TGMLP_20170528_Indianapolis500_Race.log'
 
-with open(filename, encoding='latin1') as f:
+with io.open(filename, encoding='latin1') as f:
     line = f.readline()
     cnt = 1
+    fs = {}
     while line:
         char = line[1]
-        with open('small_file_{0}'.format(line[1]), 'a+') as fout:
+        if line[1] in fs:
+            fout = fs[line[1]]
             fout.writelines(line)
+        else: 
+            fout = io.open('small_file_{0}'.format(line[1]), 'a+')
+            fs[line[1]] = fout
         line = f.readline()
         cnt += 1
         print(cnt)
+
+    for key, value in fs.iteritems():
+        value.close()
